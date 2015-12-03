18cbfa2606c29795f2b39468a2ff1ba50d500e38
Julien <julien@creativedata.fr>
2015-12-03 17:30:12 +0100

    add web api test

--- hdfs/hdfs-test.sh
+++ hdfs/hdfs-test.sh
@@ -15,5 +15,15 @@ echo -e "OK\n"
 echo -e "\nexecute cat command on hdfs\n"
 hdfs dfs -cat /tmp/test.txt 2>&1
 
+echo -e "\nexecute ls via web api on hdfs\n"
+curl -i "http://localhost:50070/webhdfs/v1/tmp/?op=LISTSTATUS"
+
+echo -e "\nexecute put file via web api on hdfs\n"
+curl -i -L -X PUT -T test.txt "http://localhost:50070/webhdfs/v1/tmp/test.txt?op=CREATE&overwrite=false"
+
+
+echo -e "\nexecute cat via web api on hdfs\n"
+curl -i -L "http://localhost:50070/webhdfs/v1/tmp/test.txt?op=OPEN"
+
 echo -e "TEST OK\n"
 
