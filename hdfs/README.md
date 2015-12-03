e274098dfed628838ea929c11f5da7d23f133f45
Julien <julien@creativedata.fr>
2015-12-03 15:41:03 +0100

    add documentation

---
+++ hdfs/README.md
@@ -0,0 +1,8 @@
+# hdfs
+
+Launch command to test
+
+`
+./hdfs-test.sh
+`
+This command put test.txt file in HDFS and after Read this file
--- hdfs/hdfs-test.sh
+++ hdfs/hdfs-test.sh
@@ -2,14 +2,14 @@
 
 echo -e "init\n"
 
-hdfs dfs -rm /tmp/test
+hdfs dfs -rm /tmp/test.txt
 
 echo -e "\nexecute ls command on hdfs\n"
 
 hdfs dfs -ls /  2>&1
 
 echo -e "\nexecute put file command on hdfs\n"
-hdfs dfs -put test /tmp/test 2>&1
+hdfs dfs -put test.txt /tmp/test.txt 2>&1
 
 echo -e "\nexecute cat command on hdfs\n"
-hdfs dfs -cat /tmp/test 2>&1
+hdfs dfs -cat /tmp/test.txt 2>&1
