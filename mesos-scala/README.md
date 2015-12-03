b29d59fc37462bd7410e3bcbc90712d8645c1cda
Julien <julien@creativedata.fr>
2015-12-03 14:32:53 +0100

    add host argument to command line

--- mesos-scala/README.md
+++ mesos-scala/README.md
@@ -9,5 +9,5 @@ This repository contains code to run shell commands in a distributed manner usin
 $MESOS_HOME refers to the mesos installation directory. Example need access mesos.so shared library which can be found
 at /usr/lib/
 
-    java -cp target/Mesos-0.0.1-SNAPSHOT.jar -Djava.library.path=/usr/lib/  fr.saagie.mesos.DistributedShell "/bin/echo hello"
+    java -cp target/Mesos-0.0.1-SNAPSHOT.jar -Djava.library.path=/usr/lib/  fr.saagie.mesos.DistributedShell localhost:5050 "/bin/echo hello"
 
--- mesos-scala/src/main/scala/fr/saagie/mesos/DistributedShell.scala
+++ mesos-scala/src/main/scala/fr/saagie/mesos/DistributedShell.scala
@@ -11,9 +11,12 @@ import org.apache.mesos.Protos.FrameworkInfo
   * Client program which will launch shell commands on cluster
   * Read README.md for example invocation.
   */
-object DistributedShell {
-
-  def main(args: Array[String]) {
+object DistributedShell extends App {
+  if (args.length != 2){
+    println("hostname commandline")
+  }else {
+    val host = args(0)
+    val command = List(args(1))
 
     val framework = FrameworkInfo.newBuilder.
       setName("DistributedShell").
@@ -26,10 +29,11 @@ object DistributedShell {
     //create instance of schedule and connect to mesos
     val scheduler = new ScalaScheduler
     //submit shell commands
-    scheduler.submitTasks(args: _*)
-    val mesosURL = "localhost:5050"
+    scheduler.submitTasks(command: _*)
+    val mesosURL = host
     val driver = new MesosSchedulerDriver(scheduler, framework, mesosURL)
     //run the driver
     driver.run()
   }
+
 }
