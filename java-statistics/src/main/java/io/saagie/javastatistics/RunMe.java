449f28580dff6e2f119cae18a19f54ef86da3a6c
Jonathan Germond <jonathan@creativedata.fr>
2016-03-07 11:51:29 +0100

    Add java-statistics project in version 5

--- java-statistics/src/main/java/io/saagie/javastatistics/RunMe.java
+++ java-statistics/src/main/java/io/saagie/javastatistics/RunMe.java
@@ -9,15 +9,16 @@ public class RunMe {
     private static Random random = new Random();
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10000000; i++) {
-            descriptiveStatistics.addValue(random.nextDouble() * 100);
+        for (int i = 0; i < Integer.valueOf(args[0]); i++) {
+            descriptiveStatistics.addValue(random.nextDouble() * (random.nextInt(200) + 1));
         }
 
-        System.out.println("Version 4");
+        System.out.println("Version 5.2");
         System.out.println("Sum : " + descriptiveStatistics.getSum());
         System.out.println("Min : " + descriptiveStatistics.getMin());
         System.out.println("Max : " + descriptiveStatistics.getMax());
         System.out.println("Mean : " + descriptiveStatistics.getMean());
         System.out.println("Median : " + descriptiveStatistics.getPercentile(50.0D));
+        System.out.println("Standard deviation : " + descriptiveStatistics.getStandardDeviation());
     }
 }
