6845bac1218babae4e73f7b28da5519d51e9b1e3
Jonathan Germond <jonathan@creativedata.fr>
2016-03-07 11:16:42 +0100

    Add java-statistics project in version 4

--- java-statistics/src/main/java/io/saagie/javastatistics/RunMe.java
+++ java-statistics/src/main/java/io/saagie/javastatistics/RunMe.java
@@ -13,10 +13,11 @@ public class RunMe {
             descriptiveStatistics.addValue(random.nextDouble() * 100);
         }
 
-        System.out.println("Version 3");
+        System.out.println("Version 4");
         System.out.println("Sum : " + descriptiveStatistics.getSum());
         System.out.println("Min : " + descriptiveStatistics.getMin());
         System.out.println("Max : " + descriptiveStatistics.getMax());
         System.out.println("Mean : " + descriptiveStatistics.getMean());
+        System.out.println("Median : " + descriptiveStatistics.getPercentile(50.0D));
     }
 }
